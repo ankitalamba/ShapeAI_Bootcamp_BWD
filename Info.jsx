6c852e3e02b8c1e8807cdf67375576e4badc4c8a
ankitalamba <72867955+ankitalamba@users.noreply.github.com>
2021-06-27 16:29:58 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>A medium through people can  learn something new</h1>
+      <p>
+        Workshops are fun as here we get to know new people and learn as well as
+        implement whatever we learn{" "}
+      </p>
+    </div>
+  );
+}
+export default Info;
